f30ea4d7aacff3708f11715e14200c34137b4dc0
Younus <mohdyounussyed100@gmail.com>
2024-02-19 16:18:48 +0530

    Merge remote-tracking branch 'origin/WithoutSecurity'
    # Conflicts:
    #	src/main/java/com/project/ecommerce_youtube/config/JWTValidator.java
    #	src/main/java/com/project/ecommerce_youtube/controller/HomeController.java
    #	src/main/java/com/project/ecommerce_youtube/controller/RatingController.java

--- src/main/java/com/project/ecommerce_youtube/controller/RatingController.java
+++ src/main/java/com/project/ecommerce_youtube/controller/RatingController.java
@@ -1,9 +1,6 @@
 package com.project.ecommerce_youtube.controller;
 
-import com.project.ecommerce_youtube.request.RatingRequest;
-import com.project.ecommerce_youtube.request.ReviewRequest;
-import com.project.ecommerce_youtube.service.ReviewServiceImplementation;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping
